21d92641ba0219cbfc45cd6f4e284871212e22e4
mybxdxyz <sma137f@ik.me>
2026-02-18 21:01:06 +0100

    Update flake inputs + configs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
-  description = "Stupid Flake";
+  description = "stupid flaek";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }: {
+
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      system = "x86_64-linux";
+
+      # Pass inputs into modules
+      specialArgs = { inherit self nixpkgs home-manager; };
+
+      modules = [
+        ./configuration.nix
+        ./greetd.nix
+
+        # Integrate Home Manager into the system
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nexus = import ./home.nix;
+        }
+      ];
     };
   };
 }
